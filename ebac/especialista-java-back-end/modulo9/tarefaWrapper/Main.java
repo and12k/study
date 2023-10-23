1adbf380be650e796f569ef70832cb86b1e38bac
Andre <andre-12-k@hotmail.com>
2023-10-23 20:35:18 -0300

    Tarefa wrapper

---
+++ ebac/especialista-java-back-end/modulo9/tarefaWrapper/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira uma variavel: ");
+
+        double i= sc.nextDouble();
+
+        Double n = (double) i;
+
+        System.out.println("Validação de conversão de variavel para classe" + n.getClass());
+
+    }
+}
